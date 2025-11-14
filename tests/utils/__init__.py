d8683750860e66a4c2f717ac66f94f3a1b7cde97
nywe4kaa <74532352+Nikita1203elko@users.noreply.github.com>
2025-11-15 01:29:21 +0500

    Delete tests directory

--- tests/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Вспомогательные функции для работы с данными и переводами
-"""
-from .data import load_data, save_data, load_translations, load_cities
-
-__all__ = ['load_data', 'save_data', 'load_translations', 'load_cities']
