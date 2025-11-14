6f2391b98d1f32b7aadec443c7fc82c31818fc4c
nywe4kaa <nikitaeliseev1203@gmail.com>
2025-11-15 01:38:57 +0500

    create-new-sait
    новые шаблоны сайта

---
+++ tests/main/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .models import UserProfile
+
+
+class UserRegistrationForm(forms.ModelForm):
+    """Форма регистрации пользователя"""
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Введите пароль'})
+    )
+    password_confirm = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Повторите пароль'})
+    )
+    patronymic = forms.CharField(
+        label='Отчество',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Введите отчество'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        labels = {
+            'username': 'Имя пользователя',
+            'email': 'Email',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Введите имя пользователя'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Введите email'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Введите имя'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Введите фамилию'}),
+        }
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+        return password_confirm
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Пользователь с таким email уже существует')
+        return email
