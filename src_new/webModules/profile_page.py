0c8b401d968663fc78579ebec580eaa4b7862824
@soonWindyK <windysoon21365@gmail.com>
2025-11-15 16:00:03 +0500

    профиль с изменением пароля

--- src_new/webModules/profile_page.py
+++ src_new/webModules/profile_page.py
@@ -12,32 +12,35 @@ def before_profile_page(request):
     return redirect('/login')
 
 
+def change_password(request, mail):
+    data_db = UsersDB_module().select_with_mail(mail=mail)
+    print(data_db)
+
+    old_pass = request.form.get('current_password')
+    new_pass = request.form.get('new_password')
+    confirm_pass = request.form.get('confirm_password')
+
+    if hasher_pass(old_pass) != data_db['user_pass']:
+        return render_template('profile.html', data_profile=data_db, error_pass='Неверный текущий пароль')
+
+    if new_pass != confirm_pass:
+        return render_template('profile.html', data_profile=data_db, error_pass='Новые пароли не совпадают')
+    else:
+        if UsersDB_module().update_password(mail=mail, password=hasher_pass(new_pass)):
+            return render_template('profile.html', data_profile=data_db, succes_pass='Пароль обновлён')
+        else:
+            return render_template('profile.html', data_profile=data_db, error_pass='Ошибка при обновлении пароля')
+
+
 def profile_page(request, mail):
     data_db = UsersDB_module().select_with_mail(mail=mail)
     data_db.pop('user_pass')
     print("Все ключи:", list(request.form.keys()))
 
     action = False
-    if request.method == 'POST':
-        action = request.form.get('action')
+    if request.method == 'POST': action = request.form.get('action')
 
     if action == 'btn-save-password':
-        data_db = UsersDB_module().select_with_mail(mail=mail)
-        print(data_db)
-
-        old_pass = request.form.get('current_password')
-        new_pass = request.form.get('new_password')
-        confirm_pass = request.form.get('confirm_password')
-
-        if hasher_pass(old_pass) != data_db['user_pass']:
-            return render_template('profile.html', data_profile=data_db, error_pass='Неверный текущий пароль')
-
-        if new_pass != confirm_pass:
-            return render_template('profile.html', data_profile=data_db, error_pass='Новые пароли не совпадают')
-        else:
-            if UsersDB_module().update_password(mail=mail, password=hasher_pass(new_pass)):
-                return render_template('profile.html', data_profile=data_db, succes_pass='Пароль обновлён')
-            else:
-                return render_template('profile.html', data_profile=data_db, error_pass='Ошибка при обновлении пароля')
+        return change_password(request=request, mail=mail)
 
     return render_template('profile.html', data_profile=data_db)
