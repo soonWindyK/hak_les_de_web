4c99fbf3be8e89a1dccddb7fcf4a322708c7263b
nywe4kaa <nikitaeliseev1203@gmail.com>
2025-11-16 17:36:36 +0500

    добавил коменты

--- src_new/manage.py
+++ src_new/manage.py
@@ -6,53 +6,54 @@ csrf = CSRFProtect()
 app = Flask(__name__, static_folder='static',static_url_path='/static')
 app.secret_key = 'your-secret-key'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
+#Рендеринг главной страницы
 @app.route('/', methods=['GET', 'POST'])
 def home(): return render_template('index.html')
 
+#Рендеринг страницы календаря
 @app.route('/calendar', methods=['GET', 'POST'])
 def calendar():
     from webModules.eventsModules.calendar import before_calendar
     return before_calendar()
 
-
+#Рендеринг страницы курсов
 @app.route('/knowledge', methods=['GET', 'POST'])
 def knowledge():
     from webModules.knowelegeModules.courses_page import before_courses
     return before_courses(request=request)
-
+#Рендеринг рендер страницы тем в курсах
 @app.route('/course/<int:course_id>', methods=['GET', 'POST'])
 def course_detail(course_id):
     from webModules.knowelegeModules.courses_page import course_detail_page
     return course_detail_page(course_id)
-
+#Рендеринг карты НКО
 @app.route('/nko/map')
 def nko_map(): return render_template('nko-map.html')
-
+#Рендеринг темы
 @app.route('/theme/<int:theme_id>', methods=['GET', 'POST'])
 def theme_detail(theme_id):
     from webModules.knowelegeModules.courses_page import theme_detail_page
     return theme_detail_page(theme_id)
-
+#Рендеринг страницы авторизации
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     from webModules.login_page import before_login_page
     return before_login_page(request=request)
-
+#Рендеринг страницы регистрации
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     from webModules.registration_page import before_reg_page
     return before_reg_page(request=request)
-
+#Рендеринг страницы новостей
 @app.route('/news', methods=['GET', 'POST'])
 def news():
     from webModules.newsModules.news import before_news
     return before_news()
-
+#Рендеринг страницы подробнее по новости
 @app.route('/news/<int:news_id>', methods=['GET', 'POST'])
 def news_detail(news_id):
     return render_template('news-detail.html')
-
+#Рендеринг страницы списка НКО
 @app.route('/nko', methods=['GET', 'POST'])
 def nko():
     from webModules.nkoModules.nko import nko_
@@ -61,18 +62,18 @@ def nko():
 # @app.route('/nko/<int:nko_id>', methods=['GET', 'POST'])
 # def nko_detail(nko_id):
 #     return render_template('nko-detail.html')
-
+#Рендеринг стираницы профиля
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
     from webModules.profile_page import before_profile_page
     return before_profile_page(request=request)
-
+#системы выхода из аккаунта
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    # Здесь будет логика выхода из системы
+
     session.clear()
     return redirect(url_for('home'))
-
+#Рендеринг панели администратора в профиле
 @app.route('/admin/panel', methods=['GET', 'POST'])
 def admin_panel():
     from webModules.adminModules.admin_panel import admin_panel
@@ -92,20 +93,20 @@ def admin_panel():
     return redirect('/')
 
 
-# Маршруты для администратора - НКО
+# Ренедеринг страницы нко в админ панели
 @app.route('/admin/nko', methods=['GET', 'POST'])
 def admin_nko_list():
     # Здесь будет логика отображения списка НКО
     from webModules.adminModules.admin_nko_list import before_admin_nko_
     return before_admin_nko_()
 
+#
+# @app.route('/profile/favorites')
+# def favorites():
+#     data_profile = UsersDB_module().select_with_mail(mail=session['username'])
+#     return render_template('favorites.html', data_profile=data_profile)
 
-@app.route('/profile/favorites')
-def favorites():
-    data_profile = UsersDB_module().select_with_mail(mail=session['username'])
-    return render_template('favorites.html', data_profile=data_profile)
-
-
+#рендеринг страницы добавления НКО
 @app.route('/nko/add', methods=['GET', 'POST'])
 def nko_add():
     from webModules.nkoModules.nko_add import before_nko_add
@@ -113,12 +114,12 @@ def nko_add():
     return before_nko_add(request=request)
 
 
-# Маршруты для администратора - Новости
+# Рендеринг страницы новостей в админ панели
 @app.route('/admin/news', methods=['GET', 'POST'])
 def admin_news_list():
     from webModules.adminModules.admin_news_list import before_admin_news
     return before_admin_news()
-
+# Рендеринг страницы добавления новости у админа
 @app.route('/admin/news/add', methods=['GET', 'POST'])
 @csrf.exempt
 def admin_news_add():
@@ -126,49 +127,49 @@ def admin_news_add():
     return before_admin_new_add()
 
 
-# Маршруты для администратора - События
+# Рендеринг страницы событий в админ панели
 @app.route('/admin/events', methods=['GET', 'POST'])
 def admin_events_list():
     from webModules.adminModules.admin_events_list import before_admin_events
     return before_admin_events()
-
+#рендеринг страницы добавления события
 @app.route('/event/add', methods=['GET', 'POST'])
 def admin_events_add():
     from webModules.eventsModules.admin_event_add import before_admin_event_add
     # Здесь будет логика добавления события
     return before_admin_event_add()
 
-# Маршруты для администратора - База знаний (Курсы)
+# Рендеринг страницы для администратора - База знаний (Курсы)
 @app.route('/admin/courses', methods=['GET', 'POST'])
 def admin_courses_list():
     from webModules.adminModules.admin_courses_list import admin_courses_list
     return admin_courses_list()
-
+#рендеринг страницы добавления курса в админ панели
 @app.route('/admin/course/add', methods=['GET', 'POST'])
 def admin_course_add():
     from webModules.adminModules.admin_courses_list import admin_course_add
     return admin_course_add(request)
-
+#Рендеринг страницы курса
 @app.route('/admin/course/<int:course_id>', methods=['GET', 'POST'])
 def admin_course_detail(course_id):
     from webModules.adminModules.admin_courses_list import admin_course_detail
     return admin_course_detail(course_id)
-
+#рендеринг страницы редактирования курса в админ панели
 @app.route('/admin/course/edit/<int:course_id>', methods=['GET', 'POST'])
 def admin_course_edit(course_id):
     from webModules.adminModules.admin_course_edit import before_admin_course_edit
     return before_admin_course_edit(course_id)
-
+#рендеринг страницы добавления темы в админ панели
 @app.route('/admin/course/<int:course_id>/theme/add', methods=['GET', 'POST'])
 def admin_theme_add(course_id):
     from webModules.adminModules.admin_courses_list import admin_theme_add
     return admin_theme_add(course_id)
-
+#редеринг страницы редактирования темы в админ панели
 @app.route('/admin/theme/<int:theme_id>/edit', methods=['GET', 'POST'])
 def admin_theme_edit(theme_id):
     from webModules.adminModules.admin_courses_list import admin_theme_edit
     return admin_theme_edit(request, theme_id)
-
+#удаление темы в админ панели
 @app.route('/admin/theme/<int:theme_id>/delete', methods=['POST'])
 def admin_theme_delete(theme_id):
     from webModules.adminModules.admin_courses_list import admin_theme_delete
@@ -196,12 +197,6 @@ def moderator_events_pending():
 def moderator_events_all():
     # Здесь будет логика отображения всех событий
     return render_template('moderator/moderator-events-all.html')
-
-# Маршруты для модератора - Статистика
-@app.route('/moderator/stats', methods=['GET', 'POST'])
-def moderator_stats():
-    # Здесь будет логика отображения статистики
-    return render_template('moderator/moderator-stats.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
