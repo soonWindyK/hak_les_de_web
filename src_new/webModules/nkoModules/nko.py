579d532f3e6d4b4bf45f19c2b1c41b66b2a12ab0
@soonWindyK <windysoon21365@gmail.com>
2025-11-16 14:01:07 +0500

    тnko filter + db worked

--- src_new/webModules/nkoModules/nko.py
+++ src_new/webModules/nkoModules/nko.py
@@ -8,23 +8,35 @@ from databaseModules.classNkoDB import NkoDB_module
 def before_nko_(request):
     return nko_(request=request)
 
+def filter(city_id):
+    if city_id == 0:
+        return NkoDB_module().get_all_nko()
+
+    nko_list = NkoDB_module().get_nko_by_city_id(city_id=city_id)
+    return nko_list
+
 
 def nko_(request):
     print(request)
-
+    cities_list = CityRegionDB_module().get_cities_list_with_region()
+    nko_list = False
+    city_selected = region_sel = ''
     if request.method == 'POST':
         action = request.form.get('action')
         if action == 'filter_go':
             city_id = int(request.form.get('city').split("_")[-1])
-            nko_list = NkoDB_module().get_nko_by_city_id(city_id=city_id)
-            print(action, city_id)
 
-    else:
+            if city_id != 0:
+                nko_list = NkoDB_module().get_nko_by_city_id(city_id=city_id)
+                city_selected = f"{cities_list[city_id-1][1]}"
+                region_sel = f"{cities_list[city_id-1][2]}"
+
+
+    if nko_list == False:
         nko_list = NkoDB_module().get_all_nko()
 
-
     cats_list = SmallFuncsDB_module().select_all_categories()
-    cities_list = CityRegionDB_module().get_cities_list_with_region()
 
 
-    return render_template('nko.html', nko_list=nko_list, cats_list=cats_list, cities_list=cities_list)
+    return render_template('nko.html', nko_list=nko_list, cats_list=cats_list, cities_list=cities_list,
+                           city_selected=city_selected, region_sel=region_sel)
